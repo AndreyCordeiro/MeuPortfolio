1871bf810584113b34c19d603242b72fbcd55a2e
Andrey <andreybr2002@hotmail.com>
2023-01-26 22:04:45 -0300

    Atualizando mensagem do template

--- funcoes/sendmessage.js
+++ funcoes/sendmessage.js
@@ -1,7 +1,7 @@
 function sendMessage() {
-    var openChat = window.open("https://wa.me/5544991165111?text=Olá%20Andrey,%20eu%20gostaria%20de%20falar%20com%20você!", '_blank');
+    window.open("https://wa.me/5544991165111?text=Olá%20Andrey,%20vi%20seu%20Portfólio%20e%20gostaria%20de%20marcar%20uma%20conversa%20com%20você!", '_blank');
 }
 
 function mailTo() {
-    window.open('mailto:andreybr2002@hotmail.com?subject=Bate-Papo&body=Olá Andrey, eu gostaria de marcar uma conversa com você!');
+    window.open('mailto:andreybr2002@hotmail.com?subject=Bate-Papo&body=Olá Andrey, vi seu Portfólio e gostaria de marcar uma conversa com você!');
 }
