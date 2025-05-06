bdf6d5e549e9f44b4072555a3733a8222d9d3a76
Andrey <andreybr2002@hotmail.com>
2025-05-05 21:30:09 -0300

    Atualizando portfólio com novas informações

--- funcoes/updatecopyright.js
+++ funcoes/updatecopyright.js
@@ -1,5 +1,5 @@
 function atualizarCopyright() {
     const copyrightDiv = document.getElementById('copyright');
 
-    copyrightDiv.textContent = '© ' + new Date().getFullYear() + ' Andrey Cordeiro';
+    copyrightDiv.textContent = '© ' + new Date().getFullYear() + ' Andrey Silva Cordeiro';
 }
